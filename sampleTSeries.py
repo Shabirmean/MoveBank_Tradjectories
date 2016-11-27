5c7ddc6dcc63a75aa6b9c613a71b0f21af993be7
Shabir Mean <shabir_tck09@hotmail.com>
2016-11-27 14:33:17 -0500

    Changes made to the sample file and added dataset used

--- sampleTSeries.py
+++ sampleTSeries.py
@@ -8,13 +8,20 @@ from keras.layers import LSTM
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error
 
+# convert an array of values into a dataset matrix
+def create_dataset(dataset, look_back=1):
+    dataX, dataY = [], []
+        for i in range(len(dataset)-look_back-1):
+            a = dataset[i:(i+look_back), 0]
+                dataX.append(a)
+                dataY.append(dataset[i + look_back, 0])
+        return numpy.array(dataX), numpy.array(dataY)
 
 # fix random seed for reproducibility
 numpy.random.seed(7)
 
 # load the dataset
-dataframe = pandas.read_csv('./tortsm.csv', usecols=[2],
-                            engine='python')
+dataframe = pandas.read_csv('./tortsm.csv', usecols=[2], engine='python')
 dataset = dataframe.values
 dataset = dataset.astype('float32')
 
@@ -23,31 +30,20 @@ scaler = MinMaxScaler(feature_range=(-1, 1))
 dataset = scaler.fit_transform(dataset)
 
 # split into train and test sets
-dataset = dataset[0:1000]
+dataset = dataset[0:10000]
 
 train_size = int(len(dataset) * 0.67)
 test_size = len(dataset) - train_size
 train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
-print(len(train), len(test))
-
-# convert an array of values into a dataset matrix
-def create_dataset(dataset, look_back=1):
-	dataX, dataY = [], []
-	for i in range(len(dataset)-look_back-1):
-		a = dataset[i:(i+look_back), 0]
-		dataX.append(a)
-		dataY.append(dataset[i + look_back, 0])
-	return numpy.array(dataX), numpy.array(dataY)
 
 # reshape into X=t and Y=t+1
-look_back = 1
+look_back = 50
 trainX, trainY = create_dataset(train, look_back)
 testX, testY = create_dataset(test, look_back)
 
 # reshape input to be [samples, time steps, features]
 trainX = numpy.reshape(trainX, (trainX.shape[0], 1, trainX.shape[1]))
 testX = numpy.reshape(testX, (testX.shape[0], 1, testX.shape[1]))
-
 
 # create and fit the LSTM network
 model = Sequential()
@@ -68,9 +64,9 @@ testY = scaler.inverse_transform([testY])
 
 # calculate root mean squared error
 trainScore = math.sqrt(mean_squared_error(trainY[0], trainPredict[:,0]))
-print('Train Score: %.4f RMSE' % (trainScore))
+print('Train Score: %.6f RMSE' % (trainScore))
 testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:,0]))
-print('Test Score: %.4f RMSE' % (testScore))
+print('Test Score: %.6f RMSE' % (testScore))
 
 # shift train predictions for plotting
 trainPredictPlot = numpy.empty_like(dataset)
