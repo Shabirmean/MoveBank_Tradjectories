fc00b85cc7d7906619932bbff41abc58a542b4a8
Shabir Mean <shabir_tck09@hotmail.com>
2016-12-09 15:17:35 -0500

    Committed with predictions related source un-commented.

--- Predictions/Hourly/Long/Long_LSTMWithMemoryBatch.py
+++ Predictions/Hourly/Long/Long_LSTMWithMemoryBatch.py
@@ -124,34 +124,34 @@ for tortoise in ['Connor', 'Isabela', 'Miriam', 'Nathalie', 'Sepp']:
     # # make predictions
     # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-    # trainPredict = model.predict(trainX, batch_size=batch_size)
-    # model.reset_states()
-    # testPredict = model.predict(testX, batch_size=batch_size)
+    trainPredict = model.predict(trainX, batch_size=batch_size)
+    model.reset_states()
+    testPredict = model.predict(testX, batch_size=batch_size)
 
-    # # invert predictions
-    # trainPredict = scaler.inverse_transform(trainPredict)
-    # trainY = scaler.inverse_transform([trainY])
-    # testPredict = scaler.inverse_transform(testPredict)
-    # testY = scaler.inverse_transform([testY])
+    # invert predictions
+    trainPredict = scaler.inverse_transform(trainPredict)
+    trainY = scaler.inverse_transform([trainY])
+    testPredict = scaler.inverse_transform(testPredict)
+    testY = scaler.inverse_transform([testY])
 
-    # # calculate root mean squared error
-    # trainScore = math.sqrt(mean_squared_error(trainY[0], trainPredict[:,0]))
-    # print('Train Score: %.6f RMSE' % (trainScore))
-    # testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:,0]))
-    # print('Test Score: %.6f RMSE' % (testScore))
+    # calculate root mean squared error
+    trainScore = math.sqrt(mean_squared_error(trainY[0], trainPredict[:,0]))
+    print('Train Score: %.6f RMSE' % (trainScore))
+    testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:,0]))
+    print('Test Score: %.6f RMSE' % (testScore))
 
-    # # shift train predictions for plotting
-    # trainPredictPlot = numpy.empty_like(dataset)
-    # trainPredictPlot[:, :] = numpy.nan
-    # trainPredictPlot[look_back:len(trainPredict)+look_back, :] = trainPredict
+    # shift train predictions for plotting
+    trainPredictPlot = numpy.empty_like(dataset)
+    trainPredictPlot[:, :] = numpy.nan
+    trainPredictPlot[look_back:len(trainPredict)+look_back, :] = trainPredict
 
-    # # shift test predictions for plotting
-    # testPredictPlot = numpy.empty_like(dataset)
-    # testPredictPlot[:, :] = numpy.nan
-    # testPredictPlot[len(trainPredict)+(look_back*2)+1:len(dataset)-1, :] = testPredict
+    # shift test predictions for plotting
+    testPredictPlot = numpy.empty_like(dataset)
+    testPredictPlot[:, :] = numpy.nan
+    testPredictPlot[len(trainPredict)+(look_back*2)+1:len(dataset)-1, :] = testPredict
 
-    # # plot baseline and predictions
-    # plt.plot(scaler.inverse_transform(dataset))
-    # plt.plot(trainPredictPlot)
-    # plt.plot(testPredictPlot)
-    # plt.show()
+    # plot baseline and predictions
+    plt.plot(scaler.inverse_transform(dataset))
+    plt.plot(trainPredictPlot)
+    plt.plot(testPredictPlot)
+    plt.show()
